be7f65515e458e98a990423bd2e3e2dcea429279
Akshita <asc6@Akshitas-iMac.local>
2017-08-16 14:49:25 +0530

    Initial Commit

---
+++ Pepperazzi_App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pepperazzi_App
+//
+//  Created by Akshita on 16/08/17.
+//  Copyright © 2017 ASC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pepperazzi_App/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Pepperazzi_App
+//
+//  Created by Akshita on 16/08/17.
+//  Copyright © 2017 ASC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
